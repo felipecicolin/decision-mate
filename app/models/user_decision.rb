2b597425aa9d1e5bcdb81eb2fc0080df2cf86c01
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-05-21 11:54:52 -0300

    Adds validations to user_decision fields

--- app/models/user_decision.rb
+++ app/models/user_decision.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class UserDecision < ApplicationRecord
+  validates :age, numericality: { only_integer: true, greater_than: 0 }, presence: true
+  validates :gender, presence: true
+  validates :educational_level, presence: true
+  validates :martial_status, presence: true
+  validates :professional_area, presence: true
+  validates :family_size, numericality: { only_integer: true, greater_or_equal_to: 0 }, presence: true
 end
