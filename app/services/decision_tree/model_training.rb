ac56088626e747ca0b87662dba3616bedad8af6c
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-05-24 21:37:58 -0300

    Adapts the service to the new functionality

--- app/services/decision_tree/model_training.rb
+++ app/services/decision_tree/model_training.rb
@@ -2,13 +2,31 @@
 
 module DecisionTree
   class ModelTraining < ApplicationService
-    def initialize(attributes, training)
+    def initialize(customer_attributes)
       super()
-      @decision_tree = DecisionTree::ID3Tree.new(attributes, training, product, :discrete)
+
+      @training = [
+        ["Male", "Baby", "Basic", "Single", "Other", "3", "Shoes"],
+        ["Female", "Kid", "Medium", "Married", "Communications", "2", "Computer"],
+        ["Male", "Teen", "Advanced", "Other", "Science and technology", "3", "Video-Game"],
+        ["Other", "Young Adult", "Very Advanced", "Married", "Arts, culture an entertainment", "2", "Mobile Phone"],
+        ["Female", "Adult", "Advanced", "Engaged", "Architecture and engineering", "3", "Watch"],
+        ["Male", "Elder", "Median", "Married", "Business, management and administration", "3", "Clothes"],
+        ["Male", "Adult", "Advanced", "Divorced", "Community and social services", "1", "Home Appliances"],
+        ["Other", "Adult", "Median", "Engaged", "Education", "3", "Auto Parts"],
+        ["Female", "Kid", "Very Advanced", "Married", "Science and technology", "3", "Books"],
+        ["Other", "Baby", "Basic", "Divorced", "Science and technology", "4", "Beauty Products"],
+      ]
+
+      @attributes = ["Gender", "Age", "Educational Level", "Marital Status", "Professional Area", "Family Size"]
+
+      @customer_attributes = customer_attributes
     end
 
     def call
-      @decision_tree.train
+      tree = DecisionTree::ID3Tree.new(@attributes, @training, "Shoes", :discrete)
+      tree.train
+      prediction = tree.predict(@customer_attributes)
     end
   end
 end
