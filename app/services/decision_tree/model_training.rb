9c78dbf875cd2a9da0d217203fe2d736e2eca26c
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-05-25 21:44:04 -0300

    Changes product categories

--- app/services/decision_tree/model_training.rb
+++ app/services/decision_tree/model_training.rb
@@ -6,16 +6,16 @@ module DecisionTree
       super()
 
       @training = [
-        ["Male", "Baby", "Basic", "Single", "Other", "3", "Shoes"],
-        ["Female", "Kid", "Medium", "Married", "Communications", "2", "Computer"],
-        ["Male", "Teen", "Advanced", "Other", "Science and technology", "3", "Video-Game"],
-        ["Other", "Young Adult", "Very Advanced", "Married", "Arts, culture an entertainment", "2", "Mobile Phone"],
-        ["Female", "Adult", "Advanced", "Engaged", "Architecture and engineering", "3", "Watch"],
+        ["Male", "Baby", "Basic", "Single", "Other", "3", "Sports and Adventure"],
+        ["Female", "Kid", "Medium", "Married", "Communications", "2", "Beauty"],
+        ["Male", "Teen", "Advanced", "Other", "Science and technology", "3", "Games"],
+        ["Other", "Young Adult", "Very Advanced", "Married", "Arts, culture an entertainment", "2", "Technology"],
+        ["Female", "Adult", "Advanced", "Engaged", "Architecture and engineering", "3", "Home Appliances"],
         ["Male", "Elder", "Median", "Married", "Business, management and administration", "3", "Clothes"],
-        ["Male", "Adult", "Advanced", "Divorced", "Community and social services", "1", "Home Appliances"],
+        ["Male", "Adult", "Advanced", "Divorced", "Community and social services", "1", "Pets"],
         ["Other", "Adult", "Median", "Engaged", "Education", "3", "Auto Parts"],
         ["Female", "Kid", "Very Advanced", "Married", "Science and technology", "3", "Books"],
-        ["Other", "Baby", "Basic", "Divorced", "Science and technology", "4", "Beauty Products"],
+        ["Other", "Baby", "Basic", "Divorced", "Science and technology", "4", "Babies"],
       ]
 
       @attributes = ["Gender", "Age", "Educational Level", "Marital Status", "Professional Area", "Family Size"]
